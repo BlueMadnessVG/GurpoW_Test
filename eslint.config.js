0088c7ec63dc4963ab86eada0d2e31712ed78daf
Christian Armenta <car_arca@hotmail.com>
2026-02-05 21:06:25 -0700

    refactor: eslint configuration with airbnb base typescript

--- eslint.config.js
+++ eslint.config.js
@@ -3,21 +3,56 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import { globalIgnores } from 'eslint/config'
+import tsParser from "@typescript-eslint/parser"
+import unusedImports from "eslint-plugin-unused-imports"
+import airbnbBase from "eslint-config-airbnb-base";
+import airbnbBaseTypescript from "eslint-config-airbnb-base-typescript";
 
-export default defineConfig([
+export default tseslint.config([
   globalIgnores(['dist']),
   {
     files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+      parser: tsParser,
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
     },
+    plugins: {
+      "@typescript-eslint": tseslint,
+      "unused-imports": unusedImports,
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh
+    },
+    rules: {
+      ...airbnbBase.rules,
+      ...airbnbBaseTypescript.rules,
+      "no-unused-vars": "off",
+      "unused-imports/no-unused-vars": [
+        "warn",
+        {
+          var: "all",
+          varsIgnorePattern: "^_",
+          args: "after-used",
+          argsIgnorePattern: "^_",
+          caughtErrorIgnorePattern: "^_" 
+        }
+      ],
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true }
+      ]
+    },
+    settings : {
+      "import/resolver": {
+        node: {
+          extensions: [".js", ".jsx", ".ts", ".tsx"]
+        }
+      }
+    }
   },
 ])
