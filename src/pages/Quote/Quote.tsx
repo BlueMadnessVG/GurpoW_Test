0b6a0a181304d2ca6d1174d0a699734f8da001e1
Christian Armenta <car_arca@hotmail.com>
2026-02-08 20:09:58 -0700

    refactor: Quote integration with form components (car, info, payment, submit) and css

--- src/pages/Quote/Quote.tsx
+++ src/pages/Quote/Quote.tsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import CarInfoSection from "./CarInfoSection/CarInfoSection";
+import CommentsSection from "./CommentsSection/CommentsSection";
+import PaymentInfoSection from "./PaymentInfoSection/PaymentInfoSection";
+import PersonalInfoSection from "./PersonalInfoSection/PersonalInfoSection";
+import SubmitSection from "./SubmitSection/SubmitSection";
+import styles from "./Quote.module.css";
+import { useQuoteForm } from "./hooks/useQuoteForm";
+import { FormProvider, useFormContext } from "react-hook-form";
+import { useQuoteFormSubmit } from "./hooks/useQuoteFormSubmit";
+
+import carImg from "@/assets/images/Fiat-card-no-bg.png";
+import type { QuoteFormData } from "@/schemas/quote.schema";
 
 function Quote() {
+  const methods = useQuoteForm();
+  const { submitForm, isSubmitting, success, error } = useQuoteFormSubmit();
+
+  const onSubmit = async (data: QuoteFormData) => {
+    console.log("Submitting data:", data);
+    const result = await submitForm(data);
+    console.log("Submit result:", result);
+  };
+
   return (
-    <div>Quote</div>
-  )
+    <div className={styles.quotePage}>
+      <header className={styles.quotePage_header}>
+        <span className={styles.quotePage_header_title}>COTIZADOR</span>
+        <span className={styles.quotePage_header_sub}>
+          ELIGE EL FIAT QUE VA CONTIGO
+        </span>
+        <h1>Elige el tuyo</h1>
+        <img src={carImg} alt="" />
+      </header>
+
+      <FormProvider {...methods}>
+        <form
+          onSubmit={methods.handleSubmit(onSubmit)}
+          className={styles.quote_Form}
+        >
+          <CarInfoSection />
+          <PersonalInfoSection />
+          <PaymentInfoSection />
+          <CommentsSection />
+          <SubmitSection />
+        </form>
+      </FormProvider>
+    </div>
+  );
 }
 
-export default Quote
+export default Quote;
